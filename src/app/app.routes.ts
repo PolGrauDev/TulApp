ddffa75c8ef61cfe849859992f5294fe2660c1ba
nil.sanca97 <nil.sanca97@gmail.com>
2026-05-05 16:37:14 +0200

    modified app.routes. now it works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'inicio',
+    component: InicioComponent,
+  }
 ];
